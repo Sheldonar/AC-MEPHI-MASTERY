54e5931ab9f691692e5aea86f2e76e18311e35eb
Sheldonar <55310753+Sheldonar@users.noreply.github.com>
2022-12-05 23:05:32 +0300

    Update hardhat.config.js

--- lab-2-bl/hardhat.config.js
+++ lab-2-bl/hardhat.config.js
@@ -7,12 +7,10 @@ const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.17",
-  defaultNetwork: "sepolia", 
-   networks: {    
-     hardhat: {},   
-     sepolia: {     
+  networks: {    
+    sepolia: {     
       url: API_URL,      
       accounts: [`0x${PRIVATE_KEY}`],   
-     }
-   }
+    }
+  }
 };
