a32e23cc5d7625fecbb2dc2f569cb403b59222d5
Sheldonar <55310753+Sheldonar@users.noreply.github.com>
2022-12-05 23:04:14 +0300

    lab 3

--- lab-2-bl/contracts/Token.sol
+++ lab-2-bl/contracts/Token.sol
@@ -4,8 +4,52 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TheSecondLab is ERC20 {
+    
+
+    // Добавить в смарт-контракт структуру данных произвольного названия, которая бы содержала поля типа: 
+    // логический, 
+    // знаковый целочисленный, 
+    // адрес 
+    // четырехбайтовый.
+
+
+    // Структура
+    struct TheStruct {
+        bool logic;
+        int number;
+        address home;
+        uint32 value;   
+    }
+
+    // Добавить отображения адресов на эту структуру
+    mapping(address => TheStruct) mapper;
+
+
+    // Функция создания структуры
+    function createStruct (address keymap, bool logic_, int number_, address home_, uint32 value_) external {
+        mapper[keymap] = TheStruct (
+            {
+               logic: logic_,
+               number: number_,
+               home: home_,
+               value: value_
+            }
+        );
+    }
+
+    // добавление в отображение
+    function getTheStruct(address addr) external view returns (TheStruct memory) {
+        return mapper[addr];
+    }   
+
+   
     constructor(uint256 initialSupply) ERC20("TheSecondLab", "TSL") {
         _mint(msg.sender, initialSupply);
     }
+
+    
+
 }
+
+
 
